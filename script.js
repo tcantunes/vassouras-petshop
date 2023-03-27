bf245b078431291dbb04caaa9acd1df0c7c63531
tcantunes <tainacantinho@hotmail.com>
2023-03-27 16:38:02 -0300

    add updates

---
+++ script.js
@@ -0,0 +1,11 @@
+function botaoContato() {
+  window.location.href = "../contato/contato.html";
+}
+
+function servico() {
+  window.location.href = "../servicos/servicos.html";
+}
+
+function contato() {
+  window.location.href = "../../contato/contato.html";
+}
